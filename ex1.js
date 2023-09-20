072c56bf33cbf8de0fc5811222726cc9c562ccb8
Brian Tuohy <BTcodez22@gmail.com>
2023-09-20 14:00:48 -0500

    ex1.upd2

--- ex1.js
+++ ex1.js
@@ -5,10 +5,12 @@ console.log("print 'this' to my global ", this);
 const user = {
     name: 'User 1',
     action: ['hello', 'goodbye'],
-    printAction: function (){
-        console.log(this)
+    printAction: function () {
+        console.log(this.action[0], this.name)
     },    
-    printAction2: () => {console.log(this)},
+    //printAction2: () => {console.log(this.action[1], this.name)}
+
+    //scope issue?
 };
 user.printAction()
 user.printAction2()
